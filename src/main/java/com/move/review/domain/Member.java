ba6b81d5390a90a9cb8467da1724aff899b0fefc
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-10-28 17:58:35 +0900

    초기 설정 - queryDSL, member entity 관련 기본 초기 설정, ResponseDto 추가

---
+++ src/main/java/com/move/review/domain/Member.java
@@ -0,0 +1,27 @@
+package com.move.review.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member extends Timestamped{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String pw;
+
+}
