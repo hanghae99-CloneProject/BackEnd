9c1b487b723df2d7aedb5fcd61b906751b326a51
dydrkflqmdl <dydrkflqmdl@naver.com>
2022-11-02 03:13:31 +0900

    Changes by puri

--- src/test/java/com/move/Bunjang/reviewApplicationTests.java
+++ src/test/java/com/move/Bunjang/reviewApplicationTests.java
@@ -3,7 +3,7 @@ package com.move.Bunjang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class reviewApplicationTests {
 
   @Test
